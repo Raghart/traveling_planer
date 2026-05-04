fd64c5e7d953fe5793ca53a123afc095ab751bd1
Raghart <rafaelrojas36@outlook.com>
2026-05-04 16:51:53 -0400

    r added rabbit connection to go CLI

---
+++ CLI/go.mod
@@ -0,0 +1,5 @@
+module github.com/Raghart/traveling_planer
+
+go 1.25.0
+
+require github.com/rabbitmq/amqp091-go v1.11.0
--- CLI/main.go
+++ CLI/main.go
@@ -3,12 +3,23 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
 	fmt.Println("Welcome to Traveling Planner!")
 	defer fmt.Println("Good traveling!")
+
+	connStr := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
