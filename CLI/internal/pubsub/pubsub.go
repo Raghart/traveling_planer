e8d6327ea44dabc3852e5b4db70fba232240dc35
Raghart <rafaelrojas36@outlook.com>
2026-05-04 21:18:09 -0400

    r added helper func to create transient queues

--- CLI/internal/pubsub/pubsub.go
+++ CLI/internal/pubsub/pubsub.go
@@ -19,3 +19,39 @@ func PublishJSON[T any](ch *ampq.Channel, exchange, key string, val T) error {
 		Body:        jsonBytes,
 	})
 }
+
+func DeclareAndBind(conn *ampq.Connection,
+	exchange, queueName, key string,
+	queueType SimpleQueueType) (*ampq.Channel, ampq.Queue, error) {
+	ch, err := conn.Channel()
+
+	if err != nil {
+		log.Fatalf("error while creating the channel: %v", err)
+	}
+
+	durable := false
+	autoDelete := false
+	exclusive := false
+
+	if queueType == DurableType {
+		durable = true
+	} else {
+		autoDelete = true
+		exclusive = true
+	}
+	queue, err := ch.QueueDeclare(queueName, durable, autoDelete, exclusive, false, nil)
+	if err != nil {
+		log.Fatalf("there was an error while creating the queue: %v", err)
+	}
+	if err = ch.QueueBind(queueName, key, exchange, false, nil); err != nil {
+		log.Fatal(err)
+	}
+	return ch, queue, nil
+}
+
+type SimpleQueueType int
+
+const (
+	DurableType = iota
+	TransientType
+)
