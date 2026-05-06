72335cc291defa85fa304b3ea4b4f21cfe0b7e8b
Raghart <rafaelrojas36@outlook.com>
2026-05-06 16:29:09 -0400

    r added documentation server func to guide

--- CLI/client/main.go
+++ CLI/client/main.go
@@ -1,83 +1,114 @@
 package main
 
 import (
-	"bufio"
-	"errors"
-	"fmt"
+	"context"
 	"log"
+	"math/rand"
 	"os"
-	"os/signal"
+	"strconv"
 	"strings"
+	"time"
 
-	"github.com/Raghart/traveling_planer/internal/pubsub"
-	"github.com/Raghart/traveling_planer/internal/routing"
-	ampq "github.com/rabbitmq/amqp091-go"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func randomString(l int) string {
+	bytes := make([]byte, l)
+	for i := 0; i < l; i++ {
+		bytes[i] = byte(randInt(65, 90))
+	}
+	return string(bytes)
+}
+
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+func fibonacciRPC(n int) (res int, err error) {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"",    // name
+		false, // durability
+		false, // delete when unused
+		true,  // exclusive
+		false, // noWait
+		nil,   // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	corrId := randomString(32)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = ch.PublishWithContext(ctx,
+		"",          // exchange
+		"rpc_queue", // routing key
+		false,       // mandatory
+		false,       // immediate
+		amqp.Publishing{
+			ContentType:   "text/plain",
+			CorrelationId: corrId,
+			ReplyTo:       q.Name,
+			Body:          []byte(strconv.Itoa(n)),
+		})
+	failOnError(err, "Failed to publish a message")
+
+	for d := range msgs {
+		if corrId == d.CorrelationId {
+			res, err = strconv.Atoi(string(d.Body))
+			failOnError(err, "Failed to convert body to integer")
+			break
+		}
+	}
+
+	return
+}
+
 func main() {
-	fmt.Println("starting testing client consumer...")
-	conn, err := ampq.Dial(routing.ConnectionStr)
-	if err != nil {
-		log.Fatalf("error while trying to connect the client: %v", err)
-	}
+	rand.Seed(time.Now().UTC().UnixNano())
 
-	username, err := ClientWelcome()
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, queue, err := pubsub.DeclareAndBind(conn,
-		routing.ExchangePerilDirect,
-		fmt.Sprintf("%s.%s", routing.TestingKey, username),
-		routing.TestingKey, pubsub.TransientType)
+	n := bodyFrom(os.Args)
 
-	if err != nil {
-		log.Fatalf("there was a problem while trying to Bind the queue")
-	}
-	fmt.Printf("queue declared: %s!", queue.Name)
+	log.Printf(" [x] Requesting fib(%d)", n)
+	res, err := fibonacciRPC(n)
+	failOnError(err, "Failed to handle RPC request")
 
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
-	fmt.Println("RabbitMQ Connection Closed")
+	log.Printf(" [.] Got %d", res)
 }
 
-func ClientWelcome() (string, error) {
-	fmt.Println("Welcome to the Peril client!")
-	fmt.Println("Please enter your username:")
-	words := GetInput()
-	if len(words) == 0 {
-		return "", errors.New("you must enter a username. goodbye")
+func bodyFrom(args []string) int {
+	var s string
+	if (len(args) < 2) || os.Args[1] == "" {
+		s = "30"
+	} else {
+		s = strings.Join(args[1:], " ")
 	}
-	username := words[0]
-	fmt.Printf("Welcome, %s!\n", username)
-	PrintClientHelp()
-	return username, nil
-}
-
-func PrintClientHelp() {
-	fmt.Println("Possible commands:")
-	fmt.Println("* move <location> <unitID> <unitID> <unitID>...")
-	fmt.Println("    example:")
-	fmt.Println("    move asia 1")
-	fmt.Println("* spawn <location> <rank>")
-	fmt.Println("    example:")
-	fmt.Println("    spawn europe infantry")
-	fmt.Println("* status")
-	fmt.Println("* spam <n>")
-	fmt.Println("    example:")
-	fmt.Println("    spam 5")
-	fmt.Println("* quit")
-	fmt.Println("* help")
-}
-
-func GetInput() []string {
-	fmt.Print("> ")
-	scanner := bufio.NewScanner(os.Stdin)
-	scanned := scanner.Scan()
-	if !scanned {
-		return nil
-	}
-	line := scanner.Text()
-	line = strings.TrimSpace(line)
-	return strings.Fields(line)
+	n, err := strconv.Atoi(s)
+	failOnError(err, "Failed to convert arg to integer")
+	return n
 }
