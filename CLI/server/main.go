7b11c0c75fb0de0f1f679594be6628ec3057fca2
Raghart <rafaelrojas36@outlook.com>
2026-05-06 18:14:31 -0400

    r setup first microserver to answer CLI

--- CLI/server/main.go
+++ CLI/server/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	"github.com/Raghart/traveling_planer/internal/routing"
 	"github.com/Raghart/traveling_planer/internal/utils"
 	ampq "github.com/rabbitmq/amqp091-go"
@@ -25,4 +29,45 @@ func main() {
 			ampq.QueueTypeArg: ampq.QueueTypeQuorum,
 		},
 	)
+	utils.FailOnError(err, "error while trying to declare the queue")
+
+	err = ch.Qos(1, 0, false)
+	utils.FailOnError(err, "error creating the QoS")
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	utils.FailOnError(err, "failed to register a consumer")
+	var forever chan struct{}
+
+	go func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		for d := range msgs {
+			err = ch.PublishWithContext(ctx,
+				"",
+				d.ReplyTo,
+				false,
+				false,
+				ampq.Publishing{
+					ContentType:   "text/plain",
+					CorrelationId: d.CorrelationId,
+					Body:          []byte("am I?"),
+				},
+			)
+			utils.FailOnError(err, "service couldn't publish answer")
+
+			d.Ack(false)
+		}
+	}()
+
+	fmt.Println("Microservice active, awaiting users requests!")
+	<-forever
 }
