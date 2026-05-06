a566833aa29810533f2738010b1081eb257ad5c8
Raghart <rafaelrojas36@outlook.com>
2026-05-06 18:54:36 -0400

    r moved CLI connection logic to helper

--- CLI/internal/pubsub/pubsub.go
+++ CLI/internal/pubsub/pubsub.go
@@ -5,7 +5,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"time"
 
+	"github.com/Raghart/traveling_planer/internal/routing"
+	"github.com/Raghart/traveling_planer/internal/utils"
 	ampq "github.com/rabbitmq/amqp091-go"
 )
 
@@ -40,6 +43,64 @@ func DeclareAndBind(conn *ampq.Connection,
 		return &ampq.Channel{}, ampq.Queue{}, fmt.Errorf("error while binding the queue: %v", err)
 	}
 	return ch, queue, nil
+}
+
+func TestingRPC() (res string) {
+	conn, err := ampq.Dial(routing.ConnectionStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+	rabbitCh, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("error while creating the channel: %v", err)
+	}
+
+	q, err := rabbitCh.QueueDeclare(
+		"",
+		false,
+		false,
+		true,
+		false,
+		nil,
+	)
+	utils.FailOnError(err, "couldn't declare the user queue")
+
+	msgs, err := rabbitCh.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	utils.FailOnError(err, "couldn't register a consumer")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+	err = rabbitCh.PublishWithContext(
+		ctx,
+		"",
+		"rpc_queue",
+		false,
+		false,
+		ampq.Publishing{
+			ContentType:   "text/plain",
+			CorrelationId: "1",
+			ReplyTo:       q.Name,
+			Body:          []byte("Who"),
+		},
+	)
+
+	for d := range msgs {
+		if d.CorrelationId == "1" {
+			res = string(d.Body)
+			break
+		}
+	}
+	return
 }
 
 type SimpleQueueType int
