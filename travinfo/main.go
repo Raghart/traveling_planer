c7badddbd63cdc64adf62d511ffb1255a1910382
Raghart <rafaelrojas36@outlook.com>
2026-05-12 18:25:08 -0400

    r added setup to recieve requests to travinfo

--- travinfo/main.go
+++ travinfo/main.go
@@ -11,8 +11,39 @@ func main() {
 	fmt.Println("Starting traveling information microservice!")
 	conn, err := ampq.Dial("amqp://guest:guest@localhost:5672/")
 	utils.FailsOnError(err, "unable to connect to RabbitMQ")
+	defer conn.Close()
 
-	if conn != nil {
-		fmt.Println("connection sucessful!")
-	}
+	ch, err := conn.Channel()
+	utils.FailsOnError(err, "unable to create a channel")
+	defer ch.Close()
+
+	queue, err := ch.QueueDeclare(
+		"travinfo-queue",
+		true,
+		false,
+		false,
+		false,
+		ampq.Table{
+			ampq.QueueTypeArg: ampq.QueueTypeQuorum,
+		},
+	)
+	utils.FailsOnError(err, "unable to creathe the queue")
+
+	err = ch.Qos(1, 0, false)
+	utils.FailsOnError(err, "unable to setup QoS")
+
+	msgs, err := ch.Consume(
+		queue.Name,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	utils.FailsOnError(err, "unable to create the channel")
+	forever := make(chan struct{})
+
+	fmt.Println("Starting travinfo, ready to recieve requests!")
+	<-forever
 }
