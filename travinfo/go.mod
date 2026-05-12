c98c17e986395673db5ce568217dbd9bfc432aae
Raghart <rafaelrojas36@outlook.com>
2026-05-12 16:48:41 -0400

    r setuping the connection rabbitMQ from the new microservice

--- travinfo/go.mod
+++ travinfo/go.mod
@@ -1,3 +1,5 @@
 module github.com/Raghart/traveling_planer/travinfo
 
 go 1.25.0
+
+require github.com/rabbitmq/amqp091-go v1.11.0 // indirect
--- travinfo/main.go
+++ travinfo/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Raghart/traveling_planer/travinfo/internal/utils"
+	ampq "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("testing")
+	fmt.Println("Starting traveling information microservice!")
+	conn, err := ampq.Dial("amqp://guest:guest@localhost:5672/")
+	utils.FailsOnError(err, "unable to connect to RabbitMQ")
+
+	if conn != nil {
+		fmt.Println("connection sucessful!")
+	}
 }
