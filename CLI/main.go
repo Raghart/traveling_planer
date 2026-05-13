fe22e1f4421bf393b47ad770ab4953bb88e94184
Raghart <rafaelrojas36@outlook.com>
2026-05-13 16:51:59 -0400

    r adding new cmd to test user asking for currency

--- CLI/main.go
+++ CLI/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/Raghart/traveling_planer/internal/pubsub"
 )
@@ -16,11 +17,22 @@ func main() {
 	for {
 		fmt.Print("CMD > ")
 		scanner.Scan()
-		switch scanner.Text() {
+		inputSlice := strings.Split(scanner.Text(), " ")
+		switch inputSlice[0] {
 		case "exit":
 			return
 		case "test":
 			fmt.Println(pubsub.TestingRPC())
+		case "curr":
+			if len(inputSlice) != 3 {
+				fmt.Println("Invalid curr cmd usage. Example: CMD > curr USD CAD")
+				continue
+			}
+
+			fromCurr := inputSlice[1]
+			toCurr := inputSlice[2]
+			result := pubsub.AskCurrency(fromCurr, toCurr)
+			fmt.Printf("Currency value from '%s' to '%s': %f\n", fromCurr, toCurr, result)
 		}
 	}
 }
