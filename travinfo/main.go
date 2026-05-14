b966b63e8f94c46291b875c576c6ef3b169e7f6c
Raghart <rafaelrojas36@outlook.com>
2026-05-14 16:45:01 -0400

    r used new helper to simplify main.go logic

--- travinfo/main.go
+++ travinfo/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
 
-	"github.com/Raghart/traveling_planer/travinfo/internal/types"
 	"github.com/Raghart/traveling_planer/travinfo/internal/utils"
 )
 
@@ -21,35 +17,7 @@ func main() {
 		for d := range msgs {
 			switch d.Type {
 			case "currency":
-				currencyData := &types.Currency{}
-				err := json.Unmarshal(d.Body, currencyData)
-
-				utils.FailsOnError(err, "unable to publish the json")
-				currencyJson := &types.CurrencyJSON{}
-
-				res, err := http.Get("https://api.fxratesapi.com/latest")
-				utils.FailsOnError(err, "unable to contact with the API")
-
-				resBody, err := io.ReadAll(res.Body)
-				utils.FailsOnError(err, "response doesn't have a body")
-
-				err = json.Unmarshal(resBody, currencyJson)
-				utils.FailsOnError(err, "unable to unmarshal json body")
-
-				dict, err := utils.StructToDict(currencyJson.Rates)
-				utils.FailsOnError(err, "unable to pack the dict")
-
-				searchValue := dict[string(currencyData.To)]
-
-				if value, isFloat := searchValue.(float64); isFloat {
-					currencyData.Value = value
-				}
-
-				err = utils.PublishJSON(ch, "", d.ReplyTo, d.CorrelationId, "currency",
-					queue, currencyData)
-				utils.FailsOnError(err, "unable to publish currency to JSON")
-
-				d.Ack(false)
+				utils.DeliverLatestCurrency(d, ch, queue)
 			default:
 				log.Println("Invalid request made!")
 				d.Ack(false)
