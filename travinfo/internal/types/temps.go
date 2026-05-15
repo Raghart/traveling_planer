cad62ca5b5b43d3f2a30398a2f1f848f685c8106
Raghart <rafaelrojas36@outlook.com>
2026-05-15 16:35:15 -0400

    r finished setting up the microserv to deliver country temp

--- travinfo/internal/types/temps.go
+++ travinfo/internal/types/temps.go
@@ -7,5 +7,25 @@ type CountryLocation struct {
 
 type CountryTemp struct {
 	Country string
-	Value   float32
+	Value   float64
+}
+
+type CountryTempJSON struct {
+	Latitude             float64 `json:"latitude"`
+	Longitude            float64 `json:"longitude"`
+	GenerationtimeMs     float64 `json:"generationtime_ms"`
+	UtcOffsetSeconds     int     `json:"utc_offset_seconds"`
+	Timezone             string  `json:"timezone"`
+	TimezoneAbbreviation string  `json:"timezone_abbreviation"`
+	Elevation            int     `json:"elevation"`
+	CurrentUnits         struct {
+		Time          string `json:"time"`
+		Interval      string `json:"interval"`
+		Temperature2M string `json:"temperature_2m"`
+	} `json:"current_units"`
+	Current struct {
+		Time          string  `json:"time"`
+		Interval      int     `json:"interval"`
+		Temperature2M float64 `json:"temperature_2m"`
+	} `json:"current"`
 }
--- travinfo/internal/utils/temp.go
+++ travinfo/internal/utils/temp.go
@@ -2,6 +2,9 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 
 	"github.com/Raghart/traveling_planer/travinfo/internal/types"
 	ampq "github.com/rabbitmq/amqp091-go"
@@ -11,6 +14,23 @@ func DeliverCountryTemperature(d ampq.Delivery, ch *ampq.Channel, queue ampq.Que
 	countryTemp := &types.CountryTemp{}
 	err := json.Unmarshal(d.Body, countryTemp)
 	FailsOnError(err, "unable to unmarshal the body")
+
+	mapLocations := LoadLocations()
+	countryLocation := mapLocations[countryTemp.Country]
+	tempURL := fmt.Sprintf("https://api.open-meteo.com/v1/forecast?latitude=%f&longitude=%f&current=temperature_2m", countryLocation.Latitude, countryLocation.Longitude)
+
+	res, err := http.Get(tempURL)
+	FailsOnError(err, fmt.Sprintf("unable to get the %s temperature", countryTemp.Country))
+
+	bodyData, err := io.ReadAll(res.Body)
+	FailsOnError(err, "unable to read the body")
+
+	countryTempJSON := &types.CountryTempJSON{}
+
+	err = json.Unmarshal(bodyData, countryTempJSON)
+	FailsOnError(err, "unable to unmarshal the JSON API data")
+
+	countryTemp.Value = countryTempJSON.Current.Temperature2M
 
 	err = PublishJSON(ch, "", d.ReplyTo, d.CorrelationId, "temperature", queue, countryTemp)
 	FailsOnError(err, "unable to publish the JSON")
