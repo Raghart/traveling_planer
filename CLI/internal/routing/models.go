fcd791b2f861b777a95524c088a34d7800d86d03
Raghart <rafaelrojas36@outlook.com>
2026-05-18 18:35:10 -0400

    r testing with tea functions

--- CLI/internal/routing/models.go
+++ CLI/internal/routing/models.go
@@ -1,5 +1,11 @@
 package routing
 
+import (
+	"fmt"
+
+	tea "charm.land/bubbletea/v2"
+)
+
 type Currency struct {
 	From  string
 	To    string
@@ -29,4 +35,58 @@ type Model struct {
 	Choices  []string
 	Cursor   int
 	Selected map[int]struct{}
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.KeyPressMsg:
+		switch msg.String() {
+
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		case "up", "k":
+			if m.Cursor > 0 {
+				m.Cursor--
+			}
+
+		case "down", "j":
+			if m.Cursor < len(m.Choices)-1 {
+				m.Cursor++
+			}
+
+		case "enter", "space":
+			_, ok := m.Selected[m.Cursor]
+			if ok {
+				delete(m.Selected, m.Cursor)
+			} else {
+				m.Selected[m.Cursor] = struct{}{}
+			}
+		}
+	}
+
+	return m, nil
+}
+
+func (m Model) View() tea.View {
+	s := "Where are you from?"
+	for idx, choice := range m.Choices {
+		cursor := " "
+		if m.Cursor == idx {
+			cursor = ">"
+		}
+
+		checked := " "
+		if _, ok := m.Selected[idx]; ok {
+			checked = ""
+		}
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+	s += "\nPress q to quit.\n"
+	return tea.NewView(s)
 }
--- CLI/server/main.go
+++ CLI/server/main.go
@@ -1,65 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"os"
 
-	"github.com/Raghart/traveling_planer/internal/pubsub"
-	"github.com/Raghart/traveling_planer/internal/routing"
+	tea "charm.land/bubbletea/v2"
 	"github.com/Raghart/traveling_planer/internal/utils"
-	ampq "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
-	conn, err := ampq.Dial(routing.ConnectionStr)
-	utils.FailOnError(err, "error while trying to connect the client")
-	defer conn.Close()
-
-	ch, err := conn.Channel()
-	utils.FailOnError(err, "error while creating the channel")
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare(
-		"rpc_queue",
-		true,
-		false,
-		false,
-		false,
-		ampq.Table{
-			ampq.QueueTypeArg: ampq.QueueTypeQuorum,
-		},
-	)
-	utils.FailOnError(err, "error while trying to declare the queue")
-
-	err = ch.Qos(1, 0, false)
-	utils.FailOnError(err, "error creating the QoS")
-
-	msgs, err := ch.Consume(
-		q.Name,
-		"",
-		false,
-		false,
-		false,
-		false,
-		nil,
-	)
-	utils.FailOnError(err, "failed to register a consumer")
-	var forever chan struct{}
-
-	go func() {
-		for d := range msgs {
-			countryData := &routing.CountryData{}
-			err = json.Unmarshal(d.Body, countryData)
-			utils.FailOnError(err, "unable to unmarshal recieved data")
-
-			countryData.IsCountry = true
-			pubsub.PublishJSON(ch, "", d.ReplyTo, "", d.CorrelationId, q, countryData)
-			utils.FailOnError(err, "service couldn't publish answer")
-
-			d.Ack(false)
-		}
-	}()
-
-	fmt.Println("Microservice active, awaiting users requests!")
-	<-forever
+	p := tea.NewProgram(utils.InitialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
