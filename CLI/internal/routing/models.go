b210db7e8d8e4d5857592dcc7c4636192bb86538
Raghart <rafaelrojas36@outlook.com>
2026-05-18 21:34:09 -0400

    r moved structs to resolve dependencies issues

--- CLI/internal/routing/models.go
+++ CLI/internal/routing/models.go
@@ -1,6 +1,11 @@
 package routing
 
 import (
+	"fmt"
+	"io"
+	"strings"
+
+	"charm.land/bubbles/v2/key"
 	"charm.land/bubbles/v2/list"
 	tea "charm.land/bubbletea/v2"
 	"charm.land/lipgloss/v2"
@@ -49,4 +54,59 @@ type Model struct {
 
 func (m Model) Init() tea.Cmd {
 	return nil
+}
+
+func NewStyles(darkBG bool) Styles {
+	var s Styles
+	s.Title = lipgloss.NewStyle().MarginLeft(2)
+	s.Item = lipgloss.NewStyle().PaddingLeft(4)
+	s.SelectItem = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	s.Pagination = list.DefaultStyles(darkBG).PaginationStyle.PaddingLeft(4)
+	s.Help = list.DefaultStyles(darkBG).HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	s.QuitText = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+	return s
+}
+
+func (m Model) UpdateStyles(isDark bool) {
+	m.Styles = NewStyles(isDark)
+	m.List.Styles.Title = m.Styles.Title
+	m.List.Styles.HelpStyle = m.Styles.Help
+	m.List.SetDelegate(ItemDelegate{Styles: &m.Styles})
+}
+
+type Item string
+
+func (i Item) FilterValue() string { return "" }
+
+type ItemDelegate struct {
+	Styles *Styles
+}
+
+func (d ItemDelegate) Height() int                             { return 1 }
+func (d ItemDelegate) Spacing() int                            { return 0 }
+func (d ItemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+func (d ItemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(Item)
+	if !ok {
+		return
+	}
+	str := fmt.Sprintf("%d. %s", index+1, i)
+	fn := d.Styles.Item.Render
+
+	if index == m.Index() {
+		fn = func(s ...string) string {
+			return d.Styles.SelectItem.Render("> " + strings.Join(s, " "))
+		}
+	}
+
+	fmt.Fprint(w, fn(str))
+}
+
+type ListKeyMap struct {
+	ToggleSpinner    key.Binding
+	ToggleTitleBar   key.Binding
+	ToggleStatusBar  key.Binding
+	TogglePagination key.Binding
+	ToggleHelpMenu   key.Binding
+	InsertItem       key.Binding
 }
--- CLI/internal/utils/tea.go
+++ CLI/internal/utils/tea.go
@@ -1,104 +1,59 @@
 package utils
 
 import (
-	"fmt"
-	"io"
-	"strings"
-
-	"charm.land/bubbles/v2/key"
 	"charm.land/bubbles/v2/list"
-	tea "charm.land/bubbletea/v2"
-	"charm.land/lipgloss/v2"
 	"github.com/Raghart/traveling_planer/internal/routing"
 )
 
-func NewStyles(darkBG bool) routing.Styles {
-	var s routing.Styles
-	s.Title = lipgloss.NewStyle().MarginLeft(2)
-	s.Item = lipgloss.NewStyle().PaddingLeft(4)
-	s.SelectItem = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
-	s.Pagination = list.DefaultStyles(darkBG).PaginationStyle.PaddingLeft(4)
-	s.Help = list.DefaultStyles(darkBG).HelpStyle.PaddingLeft(4).PaddingBottom(1)
-	s.QuitText = lipgloss.NewStyle().Margin(1, 0, 2, 4)
-	return s
-}
-
-type Item string
-
-func (i Item) FilterValue() string { return "" }
-
-type ItemDelegate struct {
-	Styles *routing.Styles
-}
-
-func (d ItemDelegate) Height() int                             { return 1 }
-func (d ItemDelegate) Spacing() int                            { return 0 }
-func (d ItemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
-func (d ItemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
-	i, ok := listItem.(Item)
-	if !ok {
-		return
-	}
-	str := fmt.Sprintf("%d. %s", index+1, i)
-	fn := d.Styles.Item.Render
-
-	if index == m.Index() {
-		fn = func(s ...string) string {
-			return d.Styles.SelectItem.Render("> " + strings.Join(s, " "))
-		}
-	}
-
-	fmt.Fprint(w, fn(str))
-}
-
-type ListKeyMap struct {
-	ToggleSpinner    key.Binding
-	ToggleTitleBar   key.Binding
-	ToggleStatusBar  key.Binding
-	TogglePagination key.Binding
-	ToggleHelpMenu   key.Binding
-	InsertItem       key.Binding
-}
-
 func InitialModel() routing.Model {
-	return routing.Model{
-		Choices: []string{
-			"Argentina",
-			"Bolivia",
-			"Brazil",
-			"Canada",
-			"Chile",
-			"Colombia",
-			"Costa Rica",
-			"Cuba",
-			"Dominica",
-			"Dominican Republic",
-			"Grenada",
-			"French Guiana",
-			"Guyana",
-			"Honduras",
-			"Saint Lucia",
-			"Mexico",
-			"Nicaragua",
-			"Panama",
-			"Peru",
-			"Puerto Rico",
-			"Paraguay",
-			"Suriname",
-			"El Salvador",
-			"Trinidad and Tobago",
-			"United States",
-			"Uruguay",
-			"Venezuela",
-			"Guatemala",
-			"Belize",
-			"Jamaica",
-			"Haiti",
-			"Bahamas",
-			"Barbados",
-			"Saint Kitts and Nevis",
-			"Antigua and Barbuda",
-		},
-		Selected: make(map[int]struct{}),
+	items := []list.Item{
+		routing.Item("Argentina"),
+		routing.Item("Bolivia"),
+		routing.Item("Brazil"),
+		routing.Item("Canada"),
+		routing.Item("Chile"),
+		routing.Item("Colombia"),
+		routing.Item("Costa Rica"),
+		routing.Item("Cuba"),
+		routing.Item("Dominica"),
+		routing.Item("Dominican Republic"),
+		routing.Item("Grenada"),
+		routing.Item("French Guiana"),
+		routing.Item("Guyana"),
+		routing.Item("Saint Lucia"),
+		routing.Item("Honduras"),
+		routing.Item("Mexico"),
+		routing.Item("Nicaragua"),
+		routing.Item("Panama"),
+		routing.Item("Peru"),
+		routing.Item("Puerto Rico"),
+		routing.Item("Paraguay"),
+		routing.Item("Suriname"),
+		routing.Item("El Salvador"),
+		routing.Item("Trinidad and Tobago"),
+		routing.Item("United States"),
+		routing.Item("Uruguay"),
+		routing.Item("Venezuela"),
+		routing.Item("Guatemala"),
+		routing.Item("Belize"),
+		routing.Item("Jamaica"),
+		routing.Item("Haiti"),
+		routing.Item("Bahamas"),
+		routing.Item("Barbados"),
+		routing.Item("Saint Kitts and Nevis"),
+		routing.Item("Antigua and Barbuda"),
 	}
+
+	const defaultWidth = 20
+
+	l := list.New(items, routing.ItemDelegate{}, defaultWidth, 14)
+	l.Title = "Where are you From?"
+	l.SetShowStatusBar(false)
+	l.SetFilteringEnabled(false)
+
+	m := routing.Model{
+		List: l,
+	}
+	m.UpdateStyles(true)
+	return m
 }
