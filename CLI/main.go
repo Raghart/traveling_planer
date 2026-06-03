05c7ad44636c681a16cb3f0c8553549b4c443dfd
Raghart <rafaelrojas36@outlook.com>
2026-06-02 20:53:42 -0400

    r moved server code to main cli file

--- CLI/main.go
+++ CLI/main.go
@@ -1,48 +1,16 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"strings"
 
-	"github.com/Raghart/traveling_planer/internal/pubsub"
-	"github.com/Raghart/traveling_planer/internal/utils"
+	tea "charm.land/bubbletea/v2"
+	"github.com/Raghart/traveling_planer/internal/routing"
 )
 
 func main() {
-	fmt.Println("Welcome to Traveling Planner!")
-	defer fmt.Println("Good traveling!")
-
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("CMD > ")
-		scanner.Scan()
-		inputSlice := strings.Split(scanner.Text(), " ")
-
-		switch inputSlice[0] {
-		case "exit":
-			return
-		case "test":
-			fmt.Println(pubsub.TestingRPC())
-		case "temp":
-			if len(inputSlice) != 2 {
-				fmt.Println("Invalid temp cmd usage. Example: CMD > temp Canada")
-			}
-			country := inputSlice[1]
-			dailyTemps := pubsub.AskTemperature(country)
-			utils.PresentWeather(dailyTemps)
-
-		case "curr":
-			if len(inputSlice) != 3 {
-				fmt.Println("Invalid curr cmd usage. Example: CMD > curr USD CAD")
-				continue
-			}
-
-			fromCurr := inputSlice[1]
-			toCurr := inputSlice[2]
-			result := pubsub.AskCurrency(fromCurr, toCurr)
-			fmt.Printf("Currency value from %s to '%s': %.2f\n", fromCurr, toCurr, result)
-		}
+	if _, err := tea.NewProgram(routing.InitialModel()).Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
 	}
 }
