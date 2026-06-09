9ccd2c03ccf4be7332c33ad93771beb7502d97ce
Raghart <rafaelrojas36@outlook.com>
2026-06-09 18:58:06 -0400

    r improved viewpoint visuals to make it prettier

--- user/internal/bubbletea/viewport.go
+++ user/internal/bubbletea/viewport.go
@@ -7,9 +7,13 @@ import (
 
 func CreateViewport() viewport.Model {
 	vp := viewport.New()
+
 	vp.Style = lipgloss.NewStyle().
 		BorderStyle(lipgloss.RoundedBorder()).
-		BorderBackground(lipgloss.Color("62"))
+		BorderForeground(lipgloss.Color("#7D56F4")).
+		Padding(1, 1).
+		MarginLeft(2).
+		MarginRight(2)
 
 	return vp
 }
