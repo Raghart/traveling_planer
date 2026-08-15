2f55e2263f88461a3aa3e09d4757e7e9005612c6
Raghart <rafaelrojas36@outlook.com>
2026-08-14 21:07:49 -0400

    r added func to retrieve all countries from API

---
+++ user/internal/utils/getJsonData.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/Raghart/traveling_planer/internal/types"
+)
+
+func GetAllCountriesData() (*types.CountriesJSON, error) {
+	res, err := http.Get("https://countries.dev/countries")
+	if err != nil {
+		return nil, fmt.Errorf("error while trying to retrieve the country data: %v", err)
+	}
+
+	jsonData := &types.CountriesJSON{}
+	bodyData, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		return nil, fmt.Errorf("error while trying to read the body: %v", err)
+	}
+
+	err = json.Unmarshal(bodyData, jsonData)
+	if err != nil {
+		return nil, fmt.Errorf("unable to unmarshal the body: %v", err)
+	}
+	return jsonData, nil
+}
