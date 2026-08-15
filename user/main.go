8315b63824a7193a165ce3e9a0457ea97ec5e678
Raghart <rafaelrojas36@outlook.com>
2026-08-14 21:04:43 -0400

    r added new util func to retrieve all the countries from the API

--- user/main.go
+++ user/main.go
@@ -1,37 +1,21 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"os"
 
 	tea "charm.land/bubbletea/v2"
 	"github.com/Raghart/traveling_planer/internal/bubbletea"
-	"github.com/Raghart/traveling_planer/internal/types"
+	"github.com/Raghart/traveling_planer/internal/utils"
 )
 
 func main() {
-	res, err := http.Get("https://countries.dev/countries")
+	countryList, err := utils.GetAllCountriesData()
 	if err != nil {
-		log.Fatalf("error while trying to retrieve the country data: %v", err)
+		log.Fatal(err)
 	}
-
-	jsonData := &types.CountriesJSON{}
-	bodyData, err := io.ReadAll(res.Body)
-
-	if err != nil {
-		log.Fatalf("error while trying to read the body: %v", err)
-	}
-
-	err = json.Unmarshal(bodyData, jsonData)
-	if err != nil {
-		log.Fatalf("unable to unmarshal the body: %v", err)
-	}
-	fmt.Printf("There are currently %d countries in the json!\n", len(*jsonData))
-
+	fmt.Printf("There are currently %d countries in the json!\n", len(*countryList))
 	return
 	if _, err := tea.NewProgram(bubbletea.InitialModel()).Run(); err != nil {
 		fmt.Println("Error running program:", err)
