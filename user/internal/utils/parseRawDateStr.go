49c1d5b33362db41aa078340031bf1f77af8652f
Raghart <rafaelrojas36@outlook.com>
2026-08-21 18:55:48 -0400

    r added switch case to return customize error to guide better the user into writing the expected date

--- user/internal/utils/parseRawDateStr.go
+++ user/internal/utils/parseRawDateStr.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func ParseRawDateStr(rawDateStr string) (string, error) {
@@ -13,10 +14,19 @@ func ParseRawDateStr(rawDateStr string) (string, error) {
 	}
 
 	parsedTravelSlice := []string{}
-	for _, dateStr := range rawTravelSlice {
+	for idx, dateStr := range rawTravelSlice {
 		dateNum, err := strconv.ParseInt(dateStr, 10, 32)
 		if err != nil {
 			return "", fmt.Errorf("unable to parse the value: %v", dateStr)
+		}
+		switch idx {
+		case 0:
+			if dateNum < int64(time.Now().Year()) {
+				return "", fmt.Errorf("Invalid year, traveling date can't be in the past!")
+			}
+			if dateNum > 3000 {
+				return "", fmt.Errorf("Invalid year, traveling date is too far into the future!")
+			}
 		}
 		parsedTravelSlice = append(parsedTravelSlice, fmt.Sprintf("%02d", dateNum))
 	}
